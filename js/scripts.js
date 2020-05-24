c9b2837b74d34612cad22783f299f762c2cd4df8
Yiran Li <v.yiran.li@gmail.com>
2020-05-23 20:40:17 -0700

    Formatted calc input expression; registered for onclick events

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,34 @@ const operate = (operator, x, y) => {
             return "Error!";
     }
 };
+
+const display = document.querySelector('#currentInput');
+
+const isOperator = (char) => (/[^0-9]/.test(char));
+
+let currentInput = display.textContent;
+
+function updateInputExpr(value) {
+    let lastVal = currentInput.slice(-1);
+    if (lastVal == 0) {
+        currentInput = `${isOperator(value)? ' ':''}${value}`;
+    } else if (isOperator(lastVal) && isOperator(value)) {
+        currentInput = currentInput.slice(0, -1) + value;
+    } else if (!isOperator(lastVal) && !isOperator(value)) {
+        currentInput += value;
+    } else {
+        currentInput += ' ' + value;
+    }
+    display.textContent = currentInput;
+}
+
+function addInput(e) {
+    // console.log(e);
+    newVal = e.target.getAttribute('value');
+    updateInputExpr(newVal);
+}
+
+// Sets up Callback Functions
+// window.addEventListener('keydown',addInput());
+const inputs = document.querySelectorAll('.input');
+inputs.forEach(input => input.addEventListener('click', addInput));
